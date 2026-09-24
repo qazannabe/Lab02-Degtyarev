a27c2f2bd3b51ba1df43dce4b27650a3b51154ba
qazannabe <qazannabe@gmail.com>
2026-09-25 01:45:24 +0300

    Lab02 add Readme.md and completed

--- obj/Debug/net10.0/Lab02-Degtyarev.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Degtyarev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Degtyarev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00cef590ef4093a0a1e7c6501abe3a186240886")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7479c1b91a7da8ee69186c14ffbc5becfdc95b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Degtyarev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Degtyarev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
